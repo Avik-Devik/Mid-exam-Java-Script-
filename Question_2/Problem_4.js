23d04a394dec3b6dd4aeeecd7a1a749004824b94
Avik Das <146101747+Avik-Devik@users.noreply.github.com>
2023-12-01 00:24:39 +0600

    Problem_4.js

--- Question_2/Problem_4.js
+++ Question_2/Problem_4.js
@@ -1,1 +1,8 @@
-Problem_4
+function isPalindrome(str) {
+    const alphanumericStr = str.toLowerCase().replace(/[\W_]/g, '');
+    const reversedStr = alphanumericStr.split('').reverse().join('');
+    return alphanumericStr === reversedStr;
+  }
+  
+  console.log(isPalindrome("racecar"));
+  console.log(isPalindrome("hello"));
