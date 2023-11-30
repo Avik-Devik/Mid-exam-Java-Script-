6d4d2909c7edaa9a41b5e88f3b240b1392fd8fbb
Avik Das <146101747+Avik-Devik@users.noreply.github.com>
2023-12-01 00:28:16 +0600

    Problem_5.js

--- Question_2/Problem_5.js
+++ Question_2/Problem_5.js
@@ -1,1 +1,12 @@
-Problem_5
+function mergeArrays(arr1, arr2) {
+    const mergedArray = arr1.concat(arr2);
+    const sortedArray = mergedArray.sort((a, b) => a - b);
+    return sortedArray;
+  }
+  
+  const array1 = [1, 3, 5, 7, 9];
+  const array2 = [2, 4, 6, 8, 10];
+  
+  const mergedAndSortedArray = mergeArrays(array1, array2);
+  console.log(mergedAndSortedArray);
+  
