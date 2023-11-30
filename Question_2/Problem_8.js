308240eb6d16fb4a4ebc789207feff73c45e944c
Avik Das <146101747+Avik-Devik@users.noreply.github.com>
2023-12-01 00:39:27 +0600

    Problem_8.js

--- Question_2/Problem_8.js
+++ Question_2/Problem_8.js
@@ -1,1 +1,46 @@
-Problem_8
+let bookTitles = [];
+let authorNames = [];
+let copiesAvailable = [];
+
+function createBookList(bookTitle, authorName, copies) {
+    bookTitles.push(bookTitle);
+    authorNames.push(authorName);
+    copiesAvailable.push(copies);
+}
+
+function AddCopies(bookTitle, addedCopies) {
+    const index = bookTitles.indexOf(bookTitle);
+    if (index !== -1) {
+        copiesAvailable[index] += addedCopies;
+        console.log(`Updated number of copies available of ${bookTitle} to ${copiesAvailable[index]}`);
+    } else {
+        console.log(`Book "${bookTitle}" not found.`);
+    }
+}
+
+function SellBook(bookTitle, soldCopies) {
+    const index = bookTitles.indexOf(bookTitle);
+    if (index !== -1) {
+        if (copiesAvailable[index] >= soldCopies) {
+            copiesAvailable[index] -= soldCopies;
+            console.log(`Updated number of copies available of ${bookTitle} to ${copiesAvailable[index]}`);
+        } else {
+            console.log(`Not enough copies available for "${bookTitle}" to sell ${soldCopies} copies.`);
+        }
+    } else {
+        console.log(`Book "${bookTitle}" not found.`);
+    }
+}
+
+function GetDetails() {
+    let details = [];
+    for (let i = 0; i < bookTitles.length; i++) {
+        details.push(`"${bookTitles[i]}" by "${authorNames[i]}" number of copies available ${copiesAvailable[i]}`);
+    }
+    return details.join(', ');
+}
+
+createBookList('Bohubrihi', 'Humayun Ahmed', 10);
+AddCopies('Bohubrihi', 5);
+SellBook('Bohubrihi', 3);
+console.log(GetDetails());
