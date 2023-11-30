555ac6218b8d7c63787f0385c12232dd4643e9b2
Avik Das <146101747+Avik-Devik@users.noreply.github.com>
2023-12-01 00:43:53 +0600

    Problem_12.js

--- Question_2/Problem_12.js
+++ Question_2/Problem_12.js
@@ -1,1 +1,22 @@
-Problem_12
+function getMaxProduct(arr) {
+    if (arr.length < 2) {
+        return "The array should have at least two elements.";
+    }
+
+    let maxProduct = Number.MIN_SAFE_INTEGER;
+
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            let product = arr[i] * arr[j];
+            if (product > maxProduct) {
+                maxProduct = product;
+            }
+        }
+    }
+
+    return maxProduct;
+}
+
+const inputArray = [2, 3, 5, 6, 7];
+const result = getMaxProduct(inputArray);
+console.log("Maximum Product:", result);
