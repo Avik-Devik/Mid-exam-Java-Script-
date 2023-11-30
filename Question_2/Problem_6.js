dfa45254a15a797ea4679096976b3d646a67b92c
Avik Das <146101747+Avik-Devik@users.noreply.github.com>
2023-12-01 00:30:33 +0600

    Problem_6.js

--- Question_2/Problem_6.js
+++ Question_2/Problem_6.js
@@ -1,1 +1,21 @@
-Problem_6
+function findShortestWord(str) {
+    const words = str.replace(/[^\w\s]/g, '').split(' ');
+
+    let shortestWord = words[0];
+    let shortestLength = shortestWord.length;
+
+    for (let i = 1; i < words.length; i++) {
+        const currentWord = words[i];
+        const currentLength = currentWord.length;
+
+        if (currentLength < shortestLength) {
+            shortestWord = currentWord;
+            shortestLength = currentLength;
+        }
+    }
+
+    return shortestWord;
+}
+
+const sentence = "The quick brown fox jumps over the lazy dog";
+console.log(findShortestWord(sentence));
