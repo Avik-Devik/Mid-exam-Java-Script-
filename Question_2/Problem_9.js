44317e5c66d3d7a8b8c33dab4f8cc5249742c045
Avik Das <146101747+Avik-Devik@users.noreply.github.com>
2023-12-01 00:39:50 +0600

    Problem_9.js

--- Question_2/Problem_9.js
+++ Question_2/Problem_9.js
@@ -1,1 +1,24 @@
-Problem_9
+function parseString(...params) {
+    let isNumber = true;
+    let sum = 0;
+    let result = '';
+
+    for (let param of params) {
+        if (!isNaN(param)) {
+            sum += parseFloat(param);
+        } else {
+            isNumber = false;
+            result += param + ' ';
+        }
+    }
+
+    if (isNumber) {
+        return sum;
+    } else {
+        return result.trim();
+    }
+}
+
+console.log(parseString("21", "24", "40"));
+console.log(parseString("Hello", "Alpha"));
+console.log(parseString("Summer", "2022"));
