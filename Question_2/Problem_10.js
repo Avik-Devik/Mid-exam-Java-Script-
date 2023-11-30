b362f33f2a8328fa7feb6ebafda344600c6a5fad
Avik Das <146101747+Avik-Devik@users.noreply.github.com>
2023-12-01 00:40:12 +0600

    Problem_10.js

--- Question_2/Problem_10.js
+++ Question_2/Problem_10.js
@@ -1,1 +1,7 @@
-Problem_11
+function PosNum(arr) {
+    return arr.filter(num => num > 0);
+}
+
+const inputArray = [2, -5, 10, -3, 8, -1, 0, 7];
+const positiveNumbers = PosNum(inputArray);
+console.log(positiveNumbers);
