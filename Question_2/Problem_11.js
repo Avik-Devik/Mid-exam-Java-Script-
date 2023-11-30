ac40ec54f14504a6d8f73e6781945b6e3b5d2841
Avik Das <146101747+Avik-Devik@users.noreply.github.com>
2023-12-01 00:41:45 +0600

    Problem_11.js

--- Question_2/Problem_11.js
+++ Question_2/Problem_11.js
@@ -1,1 +1,12 @@
-Problem_12
+function findMissingNumber(arr) {
+    const n = arr.length + 1;
+    const totalSum = (n * (n + 1)) / 2;
+    
+    const arrSum = arr.reduce((acc, curr) => acc + curr, 0);
+    
+    return totalSum - arrSum;
+}
+
+const inputArray = [1, 2, 3, 5, 6, 7, 8];
+const missingNumber = findMissingNumber(inputArray);
+console.log("The missing number is:", missingNumber);
