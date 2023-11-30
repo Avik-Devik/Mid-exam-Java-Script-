85d976ad58d57fd33fda532cb81d3515a86eec9d
Avik Das <146101747+Avik-Devik@users.noreply.github.com>
2023-12-01 00:20:22 +0600

    Problem_3.js

--- Question_2/Problem_3.js
+++ Question_2/Problem_3.js
@@ -1,1 +1,12 @@
-Problem_3
+function reverseString(str) {
+    let reversed = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversed += str[i];
+    }
+    return reversed;
+}
+
+// Example usage:
+const inputString = 'hello';
+const reversedString = reverseString(inputString);
+console.log(reversedString);
